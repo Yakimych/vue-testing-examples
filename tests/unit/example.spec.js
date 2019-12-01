4a994808fc8210242a3dfc97900489520c838ac3
Yakimych <yakimych@gmail.com>
2019-12-02 00:30:27 +0100

    Add sample test for computed, as well as mocked fetch

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,11 +2,27 @@ import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
+  beforeAll(() => {
+    global.fetch = jest.fn().mockImplementation(() => Promise.resolve());
+  });
+
   it("renders props.msg when passed", () => {
     const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     });
     expect(wrapper.text()).toMatch(msg);
+  });
+
+  it.each([
+    ["", true],
+    ["a", false],
+    ["new message", false]
+  ])("for message '%s' messageIsEmpty is %s", (msg, expectedResult) => {
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    });
+
+    expect(wrapper.vm.messageIsEmpty).toBe(expectedResult);
   });
 });
